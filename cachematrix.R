1673bd2c21540be213bb3578379537729d488132
sganesh07 <sganesh.7@gmail.com>
2015-09-26 18:41:49 +0530

    Update cachematrix.R
    loads of sync issues. .. hopefully this version goes through.

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL 
+  set <- function(y)
+  {
+    x<<-y
+    m<<-NULL
+  }
+  get <- function() x
+  setInverse <- function(Inverse) m <<-Inverse
+  
+  getInverse <- function() m
+  list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
@@ -12,4 +23,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## using the MASS package instead of the conventional solve function ... just to check 
+  library('MASS')
+  m <- x$getInverse()
+  if (!is.null(m)){
+    message("cache has an Inverse!!!")
+    return(m)
+  }
+  else message("cache is empty !! ")
+  data <- x$get()
+  m <-ginv(data,...)
+  x$setInverse(m)
+  m
+        
 }
